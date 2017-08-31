1cca0021f2b0c1e02317b3f3ac4b5548061a632b
Roman Koshkin <RomanKoshkin@Romans-MacBook-Air.local>
2017-08-31 22:55:38 +0400

    deleted

--- bMean.R
+++
@@ -1,6 +0,0 @@
-b.mean <- function(data, num) {
-  resamples <- lapply(1:num, function(i) sample(data, replace=T))
-  r.mean <- sapply(resamples, mean)
-  std.err <- sqrt(var(r.mean))
-  list(std.err=std.err, resamples=resamples, means=r.mean)
-}
